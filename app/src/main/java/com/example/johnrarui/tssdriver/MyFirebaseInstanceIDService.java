6a2731ced990aee68d03aaa65ff91426a72e2eef
john rarui <john.ngugi716@strathmore.edu>
2017-10-18 11:27:06 +0300

    commit

--- app/src/main/java/com/example/johnrarui/tssdriver/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/johnrarui/tssdriver/MyFirebaseInstanceIDService.java
@@ -1,29 +1,33 @@
-package com.example.johnrarui.tssdriver;
-
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
-import android.util.Log;
-
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-/**
- * Created by john rarui on 10/17/2017.
- */
-
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-    @Override
-    public void onTokenRefresh() {
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        //Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
-    }
-
-}
+//package com.example.johnrarui.tssdriver;
+//
+//import android.app.Service;
+//import android.content.Intent;
+//import android.os.IBinder;
+//import android.support.annotation.Nullable;
+//import android.util.Log;
+//
+//import com.google.firebase.iid.FirebaseInstanceId;
+//import com.google.firebase.iid.FirebaseInstanceIdService;
+//
+///**
+// * Created by john rarui on 10/17/2017.
+// */
+//
+//public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+//    @Override
+//    public void onTokenRefresh() {
+//        // Get updated InstanceID token.
+//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+//        //Log.d(TAG, "Refreshed token: " + refreshedToken);
+//
+//        // If you want to send messages to this application instance or
+//        // manage this apps subscriptions on the server side, send the
+//        // Instance ID token to your app server.
+//        private void sendRegistrationToServer(String refreshedToken) {
+//        }
+//        sendRegistrationToServer(refreshedToken);
+//    }
+//
+//
+//
+//}
